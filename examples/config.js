c339feea38d681de82b97b1bcc2a104757b09b78
Theo Gravity <tgravity@viv.ai>
2015-08-08 16:58:35 -0700

    Remove the api token (and invalidate the old token)

--- examples/config.js
+++ examples/config.js
@@ -1,3 +1,5 @@
+alert('fill in your access token and then remove this alert')
+
 module.exports = {
-  mapboxAccessToken: 'pk.eyJ1IjoicGVyc29uYWxpZmUiLCJhIjoiNzE1MGJkOGY2OWY2ZWZjYzk0ZjgzNDVkODc5ODQzZGUifQ.vTW_V6qqComl2Wq4hXquyQ'
+  mapboxAccessToken: 'Your token here'
 }
